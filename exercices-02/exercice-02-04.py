90627f206f1a97e2f88268a34e73ae6dae139339
sebastien <sebastien.maillet@gmail.com>
2023-06-05 16:56:24 +0200

    code de l'exercice-02

---
+++ exercices-02/exercice-02-04.py
@@ -0,0 +1,13 @@
+# exo 2.4
+# Stockez la valeur `2,7182` dans une variable nommée `number3`.
+# Arrondissez la valeur en un float à deux chiffres après la virgule et stockez le résultat dans une variable nommée `number3_rounded`.
+# Affichez le résultat.
+# Convertissez la variable `number3_rounded` en int et stockez le résultat dans une variable nommée `number3_int`.
+# Affichez le résultat.
+
+# réponse 2.4
+number3 = 2.7182
+number3_rounded = round(number3,2)
+print(number3_rounded)
+number3_int = int(number3_rounded)
+print(number3_int)
